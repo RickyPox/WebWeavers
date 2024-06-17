5dd26ffec3337e1b9af476b41b4d99d2b19885b8
Ricky <ricardojsribeiro99@gmail.com>
2024-06-17 10:53:03 +0100

    Target Blank on Links

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,9 +6,9 @@ export default function Footer (){
                 <div className="col-start-4  gap-[10px] flex flex-col">
                     <p className="text-[#FFCF47] text-left">Socials</p>
                     <div className="flex flex-col gap-[5px]">
-                        <Link href="https://webweavers.gitbook.io/whitepaper"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Whitepaper</p></Link>
-                        <Link href="https://twitter.com/webweaversnft"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Twitter</p></Link>
-                        <Link href="https://discord.gg/spiderswap"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Discord</p></Link>
+                        <Link href="https://webweavers.gitbook.io/whitepaper" target="_blank"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Whitepaper</p></Link>
+                        <Link href="https://twitter.com/webweaversnft" target="_blank"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Twitter</p></Link>
+                        <Link href="https://discord.gg/spiderswap" target="_blank"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Discord</p></Link>
                     </div>
 
                 </div>
