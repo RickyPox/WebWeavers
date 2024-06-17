39461fcb44842510da921fcaa5cf7a454ab4c6b7
Ricky <ricardojsribeiro99@gmail.com>
2024-06-17 10:51:46 +0100

    Footer update and links

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,20 +2,13 @@ import Link from "next/link";
 
 export default function Footer (){
     return(
-        <div className="gridLayout">
-                <div className="col-start-2  gap-[10px] flex flex-col 2xl:col-start-4">
-                    <p className="text-[#FFCF47] text-left">Website</p>
-                    <div className="flex flex-col gap-[5px]">
-                        <Link href="#intro"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Home</p></Link>
-                        <Link href="#whoweare"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Who we Are</p></Link>
-                        <Link href="#benefits"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Exclusive</p></Link>
-                    </div>
-                </div>
-                <div className="col-start-4  gap-[10px] flex flex-col 2xl:col-start-6">
+        <div className="flex justify-center">
+                <div className="col-start-4  gap-[10px] flex flex-col">
                     <p className="text-[#FFCF47] text-left">Socials</p>
                     <div className="flex flex-col gap-[5px]">
-                        <Link href="#"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Twitter</p></Link>
-                        <Link href="#"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Discord</p></Link>
+                        <Link href="https://webweavers.gitbook.io/whitepaper"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Whitepaper</p></Link>
+                        <Link href="https://twitter.com/webweaversnft"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Twitter</p></Link>
+                        <Link href="https://discord.gg/spiderswap"><p className="text-left text-[#EAD19F] hover:text-[#FFCF47] transition-all ease-in-out duration-200">Discord</p></Link>
                     </div>
 
                 </div>
